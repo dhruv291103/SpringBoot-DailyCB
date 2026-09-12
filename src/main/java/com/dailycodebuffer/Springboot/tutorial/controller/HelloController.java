2502c01f2febe78437421ca7500db06776c2599e
Dhruv <dhruvdpk03@gmail.com>
2026-09-13 03:03:22 +0530

    fourth

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
@@ -1,10 +1,6 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
